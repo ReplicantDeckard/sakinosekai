d0e3a242b3a216c499361b2c31c5a8e4e89464fc
ReplicantDeckard <blasteratmyside@gmail.com>
2020-09-29 17:57:25 -0700

    completed birthday wishes section

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,9 @@ const markdownShortCode = require("eleventy-plugin-markdown-shortcode");
 module.exports = function(eleventyConfig) {
     // eleventyConfig.htmlTemplateEngine("njk");
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("*.js");
-    eleventyConfig.addPassthroughCopy("*.css");
-    eleventyConfig.addWatchTarget("*.js");
-    eleventyConfig.addWatchTarget("*.css");
+    eleventyConfig.addPassthroughCopy("script.js");
+    eleventyConfig.addPassthroughCopy("styles.css");
+    eleventyConfig.addWatchTarget("script.js");
+    eleventyConfig.addWatchTarget("styles.css");
     eleventyConfig.addPlugin(markdownShortCode);
 };
---
+++ tools/build-gallery.js
@@ -0,0 +1,19 @@
+// WARNING: this will write over '_includes/_video-gallery.html'
+
+const fs = require('fs');
+const path = require('path');
+
+const videoIDFile = fs.readFileSync(path.resolve("tools/video-ids.json"), "utf8");
+const videoIDs = JSON.parse(videoIDFile);
+let htm = "";
+
+// video-ids are in newest-to-oldest order
+// videoIDs.reverse();
+
+videoIDs.forEach((id) => {
+    htm += `<li><a href="https://www.youtube.com/watch?v=${id}" target="_blank"><img src="https://img.youtube.com/vi/${id}/hqdefault.jpg" alt="thumbnail of video ${id}"></a></li>`;
+});
+
+fs.writeFileSync(path.resolve("_includes/_video-gallery.html"), htm, "utf8");
+
+console.log("gallery built in _includes/_video-gallery.html");
